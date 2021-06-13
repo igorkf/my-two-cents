806b883d42aa0de8cadb50b5bdd48355315034fe
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-12 23:56:27 -0300

    added tests for nav views

--- page/tests/test_views.py
+++ page/tests/test_views.py
@@ -8,8 +8,28 @@ class IndexViewTest(TestCase):
         response = self.client.get('/')
         status_code = response.status_code
         self.assertEqual(status_code, 200)
-        self.assertTemplateUsed(response, 'page/index.html')
 
-    def test_if_url_uses_corret_template(self):
+    def test_if_url_uses_correct_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'page/index.html')
+
+
+class WhoAmIViewTest(TestCase):
+    def test_if_url_exists_at_desired_location(self):
+        response = self.client.get('/who-am-i')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+
+class PostsViewTest(TestCase):
+    def test_if_url_exists_at_desired_location(self):
+        response = self.client.get('/posts')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+
+class ProjectsViewTest(TestCase):
+    def test_if_url_exists_at_desired_location(self):
+        response = self.client.get('/projects')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
