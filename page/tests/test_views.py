f015f3629b2f3fd211b9a9fac5421d55392a5f4c
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-15 21:30:36 -0300

    added template for posts

--- page/tests/test_views.py
+++ page/tests/test_views.py
@@ -3,6 +3,9 @@ from django.test import TestCase
 # Create your tests here.
 
 
+# TODO: write mixin
+
+
 class IndexViewTest(TestCase):
     def test_if_url_exists_at_desired_location(self):
         response = self.client.get('/')
@@ -26,6 +29,10 @@ class PostsViewTest(TestCase):
         response = self.client.get('/posts')
         status_code = response.status_code
         self.assertEqual(status_code, 200)
+
+    def test_if_url_uses_correct_template(self):
+        response = self.client.get('/posts')
+        self.assertTemplateUsed(response, 'page/posts.html')
 
 
 class ProjectsViewTest(TestCase):
--- page/views.py
+++ page/views.py
@@ -1,7 +1,46 @@
+from datetime import date
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+# TODO: change to class-based views
+
+
+fake_posts = [
+    {
+        'title': 'Why I made this site?',
+        'content': '''
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel tincidunt nisl. Vestibulum et nulla massa. Fusce molestie sed nunc in efficitur. Donec posuere vulputate tempus. Cras nec imperdiet leo. Pellentesque facilisis mauris quis nunc aliquet ultricies. Phasellus vel eleifend ligula, id laoreet neque. Morbi consectetur ante tellus, in placerat enim porta vitae. Nullam convallis, tortor sit amet semper maximus, tortor est tempus lacus, et sodales felis purus non felis. Phasellus non luctus dui.
+        Nam diam felis, elementum eu justo vitae, ornare elementum dui. Nulla mattis vehicula mi sed tincidunt. Ut fringilla semper massa at rhoncus. Phasellus turpis ante, egestas sed cursus sit amet, placerat nec est. Nulla nec odio in turpis tempus fermentum. Cras et metus velit. Ut eget accumsan urna, vitae tempor arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        ''',
+        'author': 'Igor Kuivjogi Fernandes',
+        'date': date(2021, 5, 2),
+        'tags': ['personal', 'lifestyle']
+    },
+    {
+        'title': 'How to create calculated fields in Django Models',
+        'content': '''
+        Proin hendrerit justo ac facilisis egestas. Donec convallis turpis eu eros hendrerit, ut commodo lorem luctus. Vivamus malesuada auctor sapien, et faucibus dui malesuada ac. In a purus eget lorem porttitor commodo. Suspendisse convallis velit lacinia ante facilisis lacinia.
+        Proin ultricies lorem ut fringilla tincidunt. Donec congue accumsan tortor, vel luctus nibh scelerisque vel. Aliquam congue dictum tellus faucibus ullamcorper. Proin efficitur, ante eu ornare ornare, mi elit eleifend enim, et cursus purus neque quis quam..
+        ''',
+        'author': 'Igor Kuivjogi Fernandes',
+        'date': date(2021, 6, 14),
+        'tags': ['django', 'python', 'web']
+    },
+    {
+        'title': 'Have you ever heard of the EAFP principle?',
+        'content': '''
+        Etiam felis, elementum eu justo vitae, ornare elementum dui. Nulla mattis vehicula mi sed tincidunt. Ut fringilla semper massa at rhoncus. Phasellus turpis ante, egestas sed cursus sit amet, placerat nec est. Nulla nec odio in turpis tempus fermentum. Cras et metus velit. Ut eget accumsan urna, vitae tempor arcu.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        ''',
+        'author': 'Igor Kuivjogi Fernandes',
+        'date': date(2021, 7, 15),
+        'tags': ['coding style', 'python']
+    }
+]
 
 
 def index(request):
@@ -13,7 +52,8 @@ def who_am_i(request):
 
 
 def posts(request):
-    return HttpResponse('Posts')
+    context = {'posts': fake_posts}
+    return render(request, 'page/posts.html', context)
 
 
 def projects(requests):
